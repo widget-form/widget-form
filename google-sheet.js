4dd3409c79caab85ea8b05fc7731843aaf732f85
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-04-19 11:07:09 +0000

    commit

--- google-sheet.js
+++ google-sheet.js
@@ -34,10 +34,12 @@ $("#foo").submit(function(event){
     // Callback handler that will be called on success
     request.done(function (response, textStatus, jqXHR){
         // Log a message to the console
-        console.log("Hooray, it worked!");
-        console.log(response);
-        console.log(textStatus);
-        console.log(jqXHR);
+        // console.log("Hooray, it worked!");
+        // console.log(response);
+        // console.log(textStatus);
+        // console.log(jqXHR);
+        $('.form-group').hide();
+        $('.success').show();
     });
 
     // Callback handler that will be called on failure
